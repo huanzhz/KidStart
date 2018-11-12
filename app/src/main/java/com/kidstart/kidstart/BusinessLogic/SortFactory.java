60a6460085d8bbe2b4ac473faac3c153c87e0f95
Seannyyy <43570494+Seannyyy@users.noreply.github.com>
2018-11-12 23:23:20 +0800

    equals()

--- app/src/main/java/com/kidstart/kidstart/BusinessLogic/SortFactory.java
+++ app/src/main/java/com/kidstart/kidstart/BusinessLogic/SortFactory.java
@@ -1,23 +1,20 @@
 package com.kidstart.kidstart.BusinessLogic;
 
-import com.kidstart.kidstart.BusinessLogic.Sort.SortByDistance;
-import com.kidstart.kidstart.BusinessLogic.Sort.SortByName;
-import com.kidstart.kidstart.BusinessLogic.Sort.SortByPrice;
-import com.kidstart.kidstart.BusinessLogic.Sort.SortByRating;
+import com.kidstart.kidstart.BusinessLogic.Sort.*;
 import com.kidstart.kidstart.Presentation.SortInterface;
 
 public class SortFactory {
     public SortInterface getSort(String sortType) {
-        if(sortType == "name") {
+        if(sortType.equals("name")) {
             return new SortByName();
         }
-        else if(sortType == "price"){
+        else if(sortType.equals("price")){
             return new SortByPrice();
         }
         else if(sortType == "rating"){
             return new SortByRating();
         }
-        else if(sortType == "distance"){
+        else if(sortType.equals("distance")){
             return new SortByDistance();
         }
         return null;
